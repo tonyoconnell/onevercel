418db7f83fce743390a55644cf37ad7bbdd1dfba
Anthony O'Connell <tony@one.ie>
2024-11-12 14:50:17 +0700

    Initial Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind(), partytown()]
+});
