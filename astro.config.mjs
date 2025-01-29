c6a139c290c59c27ae733f0c12e8c2d8bf532869
Anthony O'Connell <tony@one.ie>
2025-01-29 12:24:21 +0700

    Run on Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
-import cloudflare from '@astrojs/cloudflare';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [react(), tailwind(), partytown()],
@@ -28,5 +28,5 @@ export default defineConfig({
     }
   },
   output: 'server',
-  adapter: cloudflare()
+  adapter: netlify()
 });
